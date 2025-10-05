7be8d49757c0a282a83b5ac5713a9b06fd5506f4
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2025-10-05 18:50:50 -0300

    Adiciona stilo proximo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Disciplina from './Disciplina'
+
+
+function App() {
+ return <>
+    <h1>Hello, world!</h1> 
+    <Disciplina/>;
+ </>
+  
+}
+  export default App
---
+++ src/Disciplina.tsx
@@ -0,0 +1,12 @@
+
+import './Disciplina.css';
+
+function Disciplina() {
+  return (
+    <div className="disciplina">
+      <div>DCC207</div>
+      <div>Desenvolvimento Web Front-End</div>
+    </div>
+  );
+}
+export default Disciplina;
